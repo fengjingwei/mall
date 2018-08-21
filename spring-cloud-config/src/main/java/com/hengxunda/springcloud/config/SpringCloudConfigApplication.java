adb201121b13e13952e148ee2d4b959483e0116f
fengjingwei <fengjingwei@users.noreply.github.com>
2018-08-21 13:31:31 +0800

    优化配置中心

--- spring-cloud-config/src/main/java/com/hengxunda/springcloud/config/SpringCloudConfigApplication.java
+++ spring-cloud-config/src/main/java/com/hengxunda/springcloud/config/SpringCloudConfigApplication.java
@@ -3,9 +3,11 @@ package com.hengxunda.springcloud.config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableConfigServer
+@EnableEurekaClient
 public class SpringCloudConfigApplication {
 
 	public static void main(String[] args) {
