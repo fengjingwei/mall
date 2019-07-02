ec760c0964b0339f3f27a3a66c1919b5c578a98c
fengjingwei <fengjingwei@74bee.com>
2019-07-02 20:16:32 +0800

    优化代码

--- spring-cloud-nacos-config/src/main/java/com/hengxunda/springcloud/nacos/config/controller/ConfigController.java
+++ spring-cloud-nacos-config/src/main/java/com/hengxunda/springcloud/nacos/config/controller/ConfigController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RefreshScope
 public class ConfigController {
 
-    @Value("${address:}")
+    @Value("${address}")
     private String address;
 
     @GetMapping(value = "/get")
